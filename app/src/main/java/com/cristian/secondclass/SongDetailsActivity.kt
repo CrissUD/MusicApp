4c3f59941f84957215adb8cafb2510cab0a36fd5
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-09-26 09:13:27 -0500

    Creación de segunda actividad  donde se muestra una pequeña vista de detalles de una cancion

---
+++ app/src/main/java/com/cristian/secondclass/SongDetailsActivity.kt
@@ -0,0 +1,17 @@
+package com.cristian.secondclass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class SongDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_details)
+    }
+
+    fun showToast(view: View) {
+        Toast.makeText(this, "Song Activivy", Toast.LENGTH_SHORT).show()
+    }
+}
